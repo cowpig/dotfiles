fbc22a31121d5fc24b10b7fecab00634e225221b
Max <max@recoset.com>
2013-05-13 11:47:49 -0400

    .profile added to simlink

--- simlink.sh
+++ simlink.sh
@@ -2,8 +2,10 @@ rm ~/.gitconfig
 rm ~/.bashrc
 rm ~/.bash_aliases
 rm ~/.tmux.conf
+rm ~/.profile
 ln -s ~/dotfiles/.gitconfig
 ln -s ~/dotfiles/.bashrc
 ln -s ~/dotfiles/.bash_aliases
 ln -s ~/dotfiles/.tmux.conf
+ln -s ~/dotfiles/.profile
 sudo cp sublime.desktop /usr/share/applications/
