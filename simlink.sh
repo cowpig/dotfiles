583ef15df4815d317ed9b098eb0cee5a59753957
Max <max@recoset.com>
2014-03-25 16:36:13 -0400

    vim stuff

--- simlink.sh
+++ simlink.sh
@@ -3,10 +3,12 @@ rm ~/.bashrc
 rm ~/.bash_aliases
 rm ~/.tmux.conf
 rm ~/.profile
+rm ~/.vimrc
 ln -s ~/dotfiles/.gitconfig
 ln -s ~/dotfiles/.bashrc
 ln -s ~/dotfiles/.bash_aliases
 ln -s ~/dotfiles/.tmux.conf
 ln -s ~/dotfiles/.profile
 ln -s ~/dotfiles/.gdbinit
+ln -s ~/.vimrc
 sudo cp sublime.desktop /usr/share/applications/
