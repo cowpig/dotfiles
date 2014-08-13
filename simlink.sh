018d48605f3e37a9c261f6dda6f0a23674904ee9
Max <max@recoset.com>
2014-08-13 12:54:04 -0400

    removed vim stuff

--- simlink.sh
+++ simlink.sh
@@ -3,13 +3,9 @@ rm ~/.bashrc
 rm ~/.bash_aliases
 rm ~/.tmux.conf
 rm ~/.profile
-rm ~/.vimrc
-rm -rf ~/.vim
 ln -s ~/dotfiles/.gitconfig
 ln -s ~/dotfiles/.bashrc
 ln -s ~/dotfiles/.bash_aliases
 ln -s ~/dotfiles/.tmux.conf
 ln -s ~/dotfiles/.profile
 ln -s ~/dotfiles/.gdbinit
-ln -s ~/dotfiles/.vimrc
-ln -s ~/dotfiles/.vim
