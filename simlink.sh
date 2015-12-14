05277f8a6233b652e5a3b3ee86e35d410c0110b1
Max <max@recoset.com>
2015-12-14 16:20:44 -0500

    sublime config

--- simlink.sh
+++ simlink.sh
@@ -15,3 +15,10 @@ ln -s ~/dotfiles/z/z.sh
 mkdir ~/.config/terminator
 rm ~/.config/terminator/config
 ln -s ~/dotfiles/config/terminator ~/.config/terminator/config
+
+mkdir ~/.config/sublime-text-3/Packages/User/
+rm ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
+ln -s ~/dotfiles/config/sublime ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
+
+
+ Preferences.sublime-settings
