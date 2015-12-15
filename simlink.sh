68d27a7f8405ec7860a22e4eb7c6065791d672dc
Max <max@recoset.com>
2015-12-15 10:53:35 -0500

    simlink fixes

--- simlink.sh
+++ simlink.sh
@@ -3,15 +3,18 @@ rm ~/.bashrc
 rm ~/.bash_aliases
 rm ~/.tmux.conf
 rm ~/.profile
+rm ~/.gdbinit
+rm ~/.theanorc
 rm ~/z.sh
-ln -s ~/dotfiles/.gitconfig
-ln -s ~/dotfiles/.bashrc
-ln -s ~/dotfiles/.bash_aliases
-ln -s ~/dotfiles/.tmux.conf
-ln -s ~/dotfiles/.profile
-ln -s ~/dotfiles/.gdbinit
-ln -s ~/dotfiles/.theanorc
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.bashrc ~/.bashrc
+ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.profile ~/.profile
+ln -s ~/dotfiles/.gdbinit ~/.gdbinit
+ln -s ~/dotfiles/.theanorc ~/.theanorc
 ln -s ~/dotfiles/z/z.sh
+
 mkdir ~/.config/terminator
 rm ~/.config/terminator/config
 ln -s ~/dotfiles/config/terminator ~/.config/terminator/config
@@ -19,6 +22,3 @@ ln -s ~/dotfiles/config/terminator ~/.config/terminator/config
 mkdir ~/.config/sublime-text-3/Packages/User/
 rm ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
 ln -s ~/dotfiles/config/sublime ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
-
-
- Preferences.sublime-settings
