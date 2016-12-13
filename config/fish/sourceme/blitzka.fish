b5281e81a24c28c36b97b86ac1bd3d6feefa5d69
Max <max@recoset.com>
2016-12-13 15:03:00 -0500

    celery.kill

--- config/fish/sourceme/blitzka.fish
+++ config/fish/sourceme/blitzka.fish
@@ -3,8 +3,15 @@ set -x GRATER_PATH "/home/max/gits/blitzka"
 set -x GRATER_CELERY_LOG "/home/max/tmp/grater-celery.log"
 
 function blitzka.start
+    celery.kill
     cd $GRATER_PATH"/grater-django"
     nohup celery -A grater worker -l info >> $GRATER_CELERY_LOG 2>&1 &
     ./manage.py runserver
 end
 
+function celery.kill
+    for pid in (psgrep celery | awk '{print $2}')
+        kill $pid
+        echo "[X] Killed Celery worker $pid"
+    end
+end
