d35f732dd56fe74a16b8f11874e7a39a83b72e8c
Max <max@recoset.com>
2017-01-24 22:13:56 -0500

    pdbrc

--- simlink.sh
+++ simlink.sh
@@ -2,6 +2,7 @@ rm ~/.gitconfig
 rm ~/.bashrc
 rm ~/.bash_aliases
 rm ~/.tmux.conf
+rm ~/.pdbrc
 rm ~/.profile
 rm ~/.gdbinit
 rm ~/.theanorc
@@ -10,6 +11,7 @@ ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.pdbrc ~/.pdbrc
 ln -s ~/dotfiles/.profile ~/.profile
 ln -s ~/dotfiles/.gdbinit ~/.gdbinit
 ln -s ~/dotfiles/.theanorc ~/.theanorc
