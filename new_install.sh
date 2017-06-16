dfcd0f2c1c3a4abed5ee396f2b048cf908400d0b
Max <max@recoset.com>
2017-06-16 18:09:32 -0400

    npm global to home folder in new install

--- new_install.sh
+++ new_install.sh
@@ -37,4 +37,8 @@ wget https://repo.continuum.io/archive/Anaconda3-4.4.0-Linux-x86_64.sh
 bash Anaconda3-4.4.0-Linux-x86_64.sh -b
 curl https://sh.rustup.rs -sSf | sh
 
+# this is so npm global modules don't need sudo
+mkdir ~/.npm-global
+npm config set prefix '~/.npm-global'
+
 chsh -s `which fish`
