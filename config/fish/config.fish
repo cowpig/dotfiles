ab5911caa3f4ae4298df8d2238e7468b68166aa0
Max <max@recoset.com>
2017-06-16 18:07:17 -0400

    npm global to path

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -19,6 +19,9 @@ end
 if test -e $HOME/.cargo/bin
 	set PATH $PATH $HOME/.cargo/bin
 end
+if test -e $HOME/.npm_global/bin
+	set PATH $PATH $HOME/.npm_global/bin
+end
 
 for file in ~/.config/fish/sourceme/*.fish
 	source $file
